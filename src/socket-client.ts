86a7792e90d17bd9766ca6d5b1cc1c40570cc467
JaViKo500 <javikogutierrez64@gmail.com>
2024-09-07 16:32:18 -0500

    listen event clients updated and render in web

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -12,12 +12,22 @@ export const connectToServer = () => {
 
 const addListeners = ( socket: Socket ) => {
     const serverStatusLabel = document.querySelector('#server-status')!;
+    const clientUl = document.querySelector('#clients-ul')!;
 
     socket.on('connect', () => {
         serverStatusLabel.innerHTML = 'Connection established';
+
     });
 
     socket.on('disconnect', () => {
         serverStatusLabel.innerHTML = 'Disconnection established';
+    });
+
+    socket.on('clients-updated', ( clients: string[] ) => {
+        let clientsHtml = '';
+        clients.forEach((client) => {
+            clientsHtml += `<li> ${ client } </li>`;
+        });
+        clientUl.innerHTML = clientsHtml;
     });
 }

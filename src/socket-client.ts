7684806f527324119ce39ce0d096bf636a9e6e9c
JaViKo500 <javikogutierrez64@gmail.com>
2024-09-07 16:18:24 -0500

    check connection server status label

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,11 +1,23 @@
-import { Manager } from 'socket.io-client';
+import { Manager, Socket } from 'socket.io-client';
 export const connectToServer = () => {
 
     // http://localhost:3000/socket.io/socket.io.js
     const manager = new Manager('http://localhost:3000/socket.io/socket.io.js');
 
     const socket = manager.socket('/');
-    console.log('<--------------- JK Socket-client --------------->');
-    console.log(socket);
 
+    addListeners(socket);
+
+}
+
+const addListeners = ( socket: Socket ) => {
+    const serverStatusLabel = document.querySelector('#server-status')!;
+
+    socket.on('connect', () => {
+        serverStatusLabel.innerHTML = 'Connection established';
+    });
+
+    socket.on('disconnect', () => {
+        serverStatusLabel.innerHTML = 'Disconnection established';
+    });
 }
